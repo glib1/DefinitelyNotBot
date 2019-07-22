c1b2713cec9b78e8239fbc66512c5372f1e9d626
glib1 <49812077+glib1@users.noreply.github.com>
2019-07-22 16:02:38 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,4 +18,4 @@ client.on('message', message => {
 });
 
 // THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+client.login(process.env.NjAyODk2OTMxMTc3NDk2NTkw.XTYj-g.pROVRhL8HrcrU1zHGYEUS_wNESM);
